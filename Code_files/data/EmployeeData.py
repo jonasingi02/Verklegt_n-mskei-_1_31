6d8a02664895d91e807ec4302ffe40cd15b1ff26
gudagud <gudjong23@ru.is>
2023-12-07 10:16:13 +0000

    Breytingar á EmpoyeeData pilot og flightattendant writer

--- Code_files/data/EmployeeData.py
+++ Code_files/data/EmployeeData.py
@@ -44,13 +44,14 @@ class EmployeeData:
         return employees
 
 
-    def create_customer(self, employee):
+    def create_empoyee(self, employee):
+        pilots_csv_file = "Code_files/data/pilots.csv"
+        flight_attendants_csv_file = "Code_files/data/flight_attendants.csv"
+
         with open(self.file_name, 'a', newline='', encoding="utf-8") as csvfile:
             fieldnames = ["name", "kt", "phone_number", "address", "post_code", "occupation"]
             writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-            fieldnames = ["name", "kt", "phone_number", "address","post_code", "occupation"]
-            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-
+            
             writer.writerow({
                 'name': employee.name, 
                 'kt': employee.kt,
@@ -60,3 +61,35 @@ class EmployeeData:
                 'occupation': employee.occupation
             })
 
+            if employee.occupation == "flugmaður":
+                with open(self.file_name, 'a', newline='', encoding="utf-8") as csvfile:
+                    fieldnames = ["name", "kt", "phone_number", "address", "post_code", "occupation"]
+                    pilots_writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+                    pilots_writer.writerow({
+                        'name': employee.name, 
+                        'kt': employee.kt,
+                        'phone_number': employee.phone_number,
+                        'address': employee.address, 
+                        'post_code': employee.post_code,
+                        'occupation': employee.occupation
+                    })
+
+            if employee.occupation == "flugþjónn":
+                with open(self.file_name, 'a', newline='', encoding="utf-8") as csvfile:
+                    fieldnames = ["name", "kt", "phone_number", "address", "post_code", "occupation"]
+                    flight_attendants_writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+                    flight_attendants_writer.writerow({
+                        'name': employee.name, 
+                        'kt': employee.kt,
+                        'phone_number': employee.phone_number,
+                        'address': employee.address, 
+                        'post_code': employee.post_code,
+                        'occupation': employee.occupation
+                    })        
+                    
+
+            
+
+               
