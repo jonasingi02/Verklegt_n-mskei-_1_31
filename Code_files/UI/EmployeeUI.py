014c95050d143c2a6cbf6b5bac417879ff490db4
gudagud <gudjong23@ru.is>
2023-12-08 09:12:29 +0000

    A

--- Code_files/UI/EmployeeUI.py
+++ Code_files/UI/EmployeeUI.py
@@ -1,6 +1,10 @@
+from data.EmployeeData import EmployeeData
+
+
 class EmployeeUI:
     def __init__(self):
         print("inside UI")
+        self.employee_data = EmployeeData()
 
     def input_prompt(self):
         print("Employee")
