528d44efdea70b64da20ed34b6d0472d9cd7c31f
johanna11001 <johanna250400@gmail.com>
2023-12-14 17:24:15 +0000

    Ascii

--- Code_files/UI/StartUI.py
+++ Code_files/UI/StartUI.py
@@ -10,10 +10,10 @@ class StartUI:
 
     def menu_output(self):
         AsciiArt.nan_logo()
-        print(f"{'Where dividing by zero makes sense!':^50}\n")
+        print(f"{'Where dividing by zero makes sense!':^55}\n")
 
         print(
-            "\nHvað má bjóða þér að sjá?\n\n1: Flugáhöfn\n2: Vaktstjóri\n3: Ferðastjóri\n4. Helstu styrktaraðilar\nQ: Hætta"
+            "\nHvað má bjóða þér að sjá?\n\n1: Flugáhöfn\n2: Vaktstjóri\n3: Ferðastjóri\n4. Helstu styrktaraðilars\nQ: Hætta"
         )
 
     def input_prompt(self):
@@ -37,6 +37,7 @@ class StartUI:
                 if menu == "q":
                     return "q"
             elif command == "4":
+                print()
                 AsciiArt.pepsi_max()
             elif command == "q":
                 AsciiArt.airplane_3_ascii()
